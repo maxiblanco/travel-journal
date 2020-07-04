84075e175e36723fb949d66c6660a4d905249813
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-04 18:15:49 -0300

    Correct goggle type and add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const passport = require('passport');
-
 const router = express.Router();
 
-// @desc  Authenticate with Google
-// @route GET /auth/goggle
-router.get('/goggle', passport.authenticate('google', { scope: ['profile'] }));
-
-// @desc  Google authentication callback
-// @route GET /auth/google/callback)
+// @desc    Auth with Google
+// @route   GET /auth/google
 router.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }, (req, res) => {
-    res.redirect('/dashboard');
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile'],
   })
 );
+
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
+
+// @description Logout user from Google
+// @route /auth/Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
