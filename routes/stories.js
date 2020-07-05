cf86e5825c950023ab78b65a30942ed7919bdeaa
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-04 21:53:56 -0300

    Show all stories route

--- routes/stories.js
+++ routes/stories.js
@@ -22,3 +22,23 @@ router.post('/', ensureAuth, async (req, res) => {
     res.render('error/500');
   }
 });
+
+// @desc  Show all stories
+// @route GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean();
+    console.log(stories);
+    res.render('stories/index', {
+      stories,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
+  }
+});
+
+module.exports = router;
