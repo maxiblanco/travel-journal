398351036bd7a6aceb819d68a47761b3124295bf
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-04 21:53:36 -0300

    Get add new story route

--- routes/stories.js
+++ routes/stories.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const { ensureAuth } = require('../middleware/auth');
+const router = express.Router();
+
+const Story = require('../models/Story');
+
+// @desc  Show add page
+// @route GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add');
+});
+
 // @desc  Process add form
 // @route POST /stories
 router.post('/', ensureAuth, async (req, res) => {
